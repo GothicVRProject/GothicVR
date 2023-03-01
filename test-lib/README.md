a1a2c5a622c2b9094e961375328ec595fc81c6bf
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2023-03-01 08:16:57 +0100

    Needed to link stdlib statically as it wasnÄt found at runtime when compiling with mingw64. Works now. Wohooo!

--- test-lib/README.md
+++ test-lib/README.md
@@ -1,5 +1,16 @@
+## Compiling DLL
 
-```sh
+## Pre-setup
+(tested mingw-w64 version: 12.2.0)
+
+```powershell
+choco install mingw
+```
+
+
+## Compile
+
+```powershell
 cmake -G "MinGW Makefiles" -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build
 ```
--- test-lib/main.cc
+++ test-lib/main.cc
@@ -7,9 +7,17 @@
 
 int main(int argc, char** argv) {
 	// auto buf = phoenix::buffer::mmap(argv[1]);
+	std::cout << "Test" << "\n";
 
 
-    auto vdf = phoenix::vdf_file::open("C:\\Prfdxsogram Files (x86)\\Steam\\steamapps\\common\\Gothic\\Data\\speech_babe_speech_engl.VDF");
+    auto vdf = phoenix::vdf_file::open("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Gothic\\Data\\speech_babe_speech_engl.VDF");
+	auto& header = vdf.header;
+
+	std::cout << "Description: " << header.comment << "\n"
+	          << "Timestamp (Unix): " << header.timestamp << "\nEntries:\n";
+
+	int n;
+	std::cin >> n;
 
 	return 0;
 }
