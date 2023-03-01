64a4cc026dc2d25808bf2c7a897c6c2a5c424855
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2023-03-01 17:50:23 +0100

    Successfully parsing all the VDF files, merging into one container, and checking if a specific file (e.g. world.zen) is included.

--- Assets/unZENity-VR/Scripts/DllImportTest.cs
+++ Assets/unZENity-VR/Scripts/DllImportTest.cs
@@ -12,19 +12,27 @@ namespace UZVR
         private const string G1Dir = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Gothic\\";
 
 
-        [DllImport(DLLNAME)] private static extern IntPtr getVDFHeader(string vdfPath);
-        [DllImport(DLLNAME)] private static extern string getHeaderComment(IntPtr vdfHeader);
-        [DllImport(DLLNAME)] private static extern void disposeHeader(IntPtr vdfHeader);
-        
+        [DllImport(DLLNAME)] private static extern IntPtr createVDFContainer();
+        [DllImport(DLLNAME)] private static extern void addVDFToContainer(IntPtr vdfContainer, string vdfPath);
+        [DllImport(DLLNAME)] private static extern IntPtr getVDFEntry(IntPtr vdfContainer, string name);
+        [DllImport(DLLNAME)] private static extern void disposeVDFContainer(IntPtr vdfContainer);
+
+        private IntPtr vdfContainer;
 
         void Start()
         {
+            vdfContainer = createVDFContainer();
+
             var vdfPaths = GetVDFPaths();
 
             foreach (var vdfPath in vdfPaths)
             {
                HandleVDF(vdfPath);
             }
+
+            var found = findVDFByName("world.zen");
+
+            disposeVDFContainer(vdfContainer);
         }
 
 
@@ -37,12 +45,14 @@ namespace UZVR
 
         private void HandleVDF(string vdfPath)
         {
-            IntPtr vdfHeader = getVDFHeader(vdfPath);
-            var comment = getHeaderComment(vdfHeader);
-            // FIXME When reading a c_str/char* from DLL, the dispose will kill Unity app. Why?
-            // disposeHeader(vdfHeader);
+            addVDFToContainer(vdfContainer, vdfPath);
+        }
 
-            Debug.Log(comment);
+        private bool findVDFByName(string name)
+        {
+            IntPtr found = getVDFEntry(vdfContainer, name);
+
+            return found != IntPtr.Zero;
         }
     }
 }
