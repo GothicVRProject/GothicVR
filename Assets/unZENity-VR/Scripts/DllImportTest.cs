20edfa7d8c93e1ebf8146dd5d5a8ccb541e2de84
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2023-03-01 16:00:18 +0100

    Read all VDF files and list header data.

--- Assets/unZENity-VR/Scripts/DllImportTest.cs
+++ Assets/unZENity-VR/Scripts/DllImportTest.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 using UnityEngine;
 
@@ -6,15 +8,39 @@ namespace UZVR
 {
     public class DllImportTest : MonoBehaviour
     {
-        [DllImport("phoenix-csharp-wrapper")]
-        private static extern IntPtr getVDFHeader();
-        [DllImport("phoenix-csharp-wrapper")]
-        private static extern string getHeaderComment(IntPtr vdfHeader);
+        private const string DLLNAME = "phoenix-csharp-wrapper";
+        private const string G1Dir = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Gothic\\";
+
+
+        [DllImport(DLLNAME)] private static extern IntPtr getVDFHeader(string vdfPath);
+        [DllImport(DLLNAME)] private static extern string getHeaderComment(IntPtr vdfHeader);
+        [DllImport(DLLNAME)] private static extern void disposeHeader(IntPtr vdfHeader);
+        
 
         void Start()
         {
-            IntPtr vdfHeader = getVDFHeader();
+            var vdfPaths = GetVDFPaths();
+
+            foreach (var vdfPath in vdfPaths)
+            {
+               HandleVDF(vdfPath);
+            }
+        }
+
+
+
+        private string[] GetVDFPaths()
+        {
+            return Directory.GetFiles(G1Dir + "/Data", "*.vdf");
+        }
+
+
+        private void HandleVDF(string vdfPath)
+        {
+            IntPtr vdfHeader = getVDFHeader(vdfPath);
             var comment = getHeaderComment(vdfHeader);
+            // FIXME When reading a c_str/char* from DLL, the dispose will kill Unity app. Why?
+            // disposeHeader(vdfHeader);
 
             Debug.Log(comment);
         }
