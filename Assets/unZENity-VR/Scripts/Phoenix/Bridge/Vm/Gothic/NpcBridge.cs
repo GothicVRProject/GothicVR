aebf454dd4c03e5b57bb295ab27b4c1e61af506f
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2023-03-30 15:46:14 +0200

    Got rid of NPC map within C++ bridge. It's now a pointer stored on managed code and later disposed if game closes.

--- Assets/unZENity-VR/Scripts/Phoenix/Bridge/Vm/Gothic/NpcBridge.cs
+++ Assets/unZENity-VR/Scripts/Phoenix/Bridge/Vm/Gothic/NpcBridge.cs
@@ -12,47 +12,57 @@ namespace UZVR.Phoenix.Bridge.Vm.Gothic
     {
         private const string DLLNAME = PhoenixBridge.DLLNAME;
         private readonly VmBridge _vmBridge;
+        private IntPtr _npcsContainer = IntPtr.Zero;
 
         // Basic
-        [DllImport(DLLNAME)] private static extern IntPtr vmGothicNpcInitInstance(IntPtr vm, int instanceId);
-        [DllImport(DLLNAME)] private static extern IntPtr vmGothicNpcInitInstanceByName(IntPtr vm, string instanceName);
+        [DllImport(DLLNAME)] private static extern IntPtr vmGothicNpcInitInstancesContainer();
+        [DllImport(DLLNAME)] private static extern void vmGothicNpcDisposeInstancesContainer(IntPtr npcsContainer);
+
+        [DllImport(DLLNAME)] private static extern IntPtr vmGothicNpcInitInstance(IntPtr vm, IntPtr npcsContainer, int instanceId);
+        [DllImport(DLLNAME)] private static extern IntPtr vmGothicNpcInitInstanceByName(IntPtr vm, IntPtr npcsContainer, string instanceName);
         [DllImport(DLLNAME)] private static extern uint vmGothicNpcGetSymbolIndex(IntPtr npcPtr);
-        [DllImport(DLLNAME)] private static extern void vmGothicNpcCallFunctionByIndex(IntPtr vm, int index, IntPtr npcPtr);
+        [DllImport(DLLNAME)] private static extern void vmGothicNpcCallFunctionByIndex(IntPtr vm, IntPtr npcsContainer, int index, IntPtr npcPtr);
 
         // Specific
         [DllImport(DLLNAME)] private static extern int vmGothicNpcGetRoutine(IntPtr npcPtr);
         [DllImport(DLLNAME)] private static extern int vmGothicNpcGetNameSize(IntPtr npcPtr);
         [DllImport(DLLNAME)] private static extern void vmGothicNpcGetName(IntPtr npc, StringBuilder name);
 
-        [DllImport(DLLNAME)] private static extern void vmGothicNpcDispose(IntPtr npcPtr);
+        [DllImport(DLLNAME)] private static extern void vmGothicNpcDispose(IntPtr npcsContainer, IntPtr npcPtr);
 
 
         public NpcBridge(VmBridge vmBridge)
         {
             _vmBridge = vmBridge;
+            _npcsContainer = vmGothicNpcInitInstancesContainer();
 
             RegisterNpcCallbacks();
+        }
+
+        ~NpcBridge()
+        {
+            vmGothicNpcDisposeInstancesContainer(_npcsContainer);
         }
 
         // FIXME needs to be called when an NPC is destroyed from Unity scene to free memory on C++ side.
         public void DisposeNpc(IntPtr npcPtr)
         {
-            vmGothicNpcDispose(npcPtr);
+            vmGothicNpcDispose(_npcsContainer, npcPtr);
         }
 
         public void CallFunction(int index, IntPtr npcInstance)
         {
-            vmGothicNpcCallFunctionByIndex(_vmBridge.VmPtr, index, npcInstance);
+            vmGothicNpcCallFunctionByIndex(_vmBridge.VmPtr, _npcsContainer, index, npcInstance);
         }
 
         public IntPtr InitNpcInstance(int instanceId)
         {
-            return vmGothicNpcInitInstance(_vmBridge.VmPtr, instanceId);
+            return vmGothicNpcInitInstance(_vmBridge.VmPtr, _npcsContainer, instanceId);
         }
 
         public IntPtr InitNpcInstance(string instanceName)
         {
-            return vmGothicNpcInitInstanceByName(_vmBridge.VmPtr, instanceName);
+            return vmGothicNpcInitInstanceByName(_vmBridge.VmPtr, _npcsContainer, instanceName);
         }
 
         public uint GetNpcSymbolId(IntPtr npc)
