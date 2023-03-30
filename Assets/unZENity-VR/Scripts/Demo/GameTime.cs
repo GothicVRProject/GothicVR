2c0e567c39f362efac7cc678faf581ed06686827
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2023-03-30 16:34:15 +0200

    #4 - Added sun light.

--- Assets/unZENity-VR/Scripts/Demo/GameTime.cs
+++ Assets/unZENity-VR/Scripts/Demo/GameTime.cs
@@ -9,7 +9,11 @@ namespace UZVR.Demo
     {
         private static DateTime MIN_TIME = new(1, 1, 1, 0, 0, 0);
         private static DateTime MAX_TIME = new(1, 1, 1, 23, 59, 59);
-        private DateTime time = MIN_TIME;
+        private DateTime time = new(1, 1, 1, 12, 0, 0);
+        private static readonly float GAMETIME_SPEED = 0.5f;
+
+        public GameObject sun;
+
 
         void Start()
         {
@@ -34,8 +38,15 @@ namespace UZVR.Demo
                 if (time > MAX_TIME)
                     time = MIN_TIME;
 
-                Debug.Log("Current Daytime: " + time);
-                yield return new WaitForSeconds(1f);
+
+                // 270° = midnight
+                // 90° = noon
+
+                var xRotation = 270f + (15f * ((float)time.Hour + (float)time.Minute / 60f));
+                sun.transform.eulerAngles = new(xRotation, 0, 0);
+
+
+                yield return new WaitForSeconds(GAMETIME_SPEED);
             }
         }
     }
