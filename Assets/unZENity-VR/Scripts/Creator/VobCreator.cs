da4bbc04cec82fd6c27c80d62844ae3a6930a6e8
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2023-04-20 13:06:11 +0200

    Items are now also rendered with submeshes!

--- Assets/unZENity-VR/Scripts/Creator/VobCreator.cs
+++ Assets/unZENity-VR/Scripts/Creator/VobCreator.cs
@@ -12,7 +12,7 @@ using UZVR.Util;
 
 namespace UZVR.Creator
 {
-    public class VobCreator: SingletonBehaviour<VobCreator>
+    public class VobCreator : SingletonBehaviour<VobCreator>
     {
         // Cache helped speed up loading of G1 world textures from 870ms to 230 (~75% speedup)
         private Dictionary<string, Texture2D> cachedTextures = new();
@@ -83,73 +83,71 @@ namespace UZVR.Creator
 
         private void PrepareMeshRenderer(MeshRenderer meshRenderer, PxMRMData mrmData)
         {
-            if (mrmData.materials.Length != 1)
-                throw new ArgumentOutOfRangeException("Currently it's only supported to have exact 1 material for VobMRMs.");
+            //if (mrmData.materials.Length != 1)
+            //    throw new ArgumentOutOfRangeException("Currently it's only supported to have exact 1 material for VobMRMs.");
 
-            var standardShader = Shader.Find("Standard");
-            var material = new Material(standardShader);
-            var materialData = mrmData.materials.First();
+            var finalMaterials = new List<Material>(mrmData.subMeshes.Length);
 
-            meshRenderer.material = material;
-
-            // No texture to add.
-            if (materialData.texture == "")
-                return;
-
-            // Load texture for the first time.
-            if (!cachedTextures.TryGetValue(materialData.texture, out Texture2D cachedTexture))
+            foreach (var subMesh in mrmData.subMeshes)
             {
-                // FIXME - There might be more textures to load compressed. Please check for sake of performance!
-                var pxTexture = PxTexture.GetTextureFromVdf(
-                    PhoenixBridge.VdfsPtr,
-                    materialData.texture,
-                    PxTexture.Format.tex_dxt1, PxTexture.Format.tex_dxt5);
+                var standardShader = Shader.Find("Standard");
+                var material = new Material(standardShader);
+                var materialData = subMesh.material;
 
-                // No texture found
-                if (pxTexture == null)
-                {
-                    Debug.LogWarning($"Texture {materialData.texture} couldn't be found.");
+                meshRenderer.material = material;
+
+                // No texture to add.
+                if (materialData.texture == "")
                     return;
-                }
 
-                var format = pxTexture.format.AsUnityTextureFormat();
-                if (format == 0)
+                // Load texture for the first time.
+                if (!cachedTextures.TryGetValue(materialData.texture, out Texture2D cachedTexture))
                 {
-                    Debug.LogWarning($"Format >{pxTexture.format}< is not supported or not yet tested to work with Unity:");
-                    return;
+                    // FIXME - There might be more textures to load compressed. Please check for sake of performance!
+                    var pxTexture = PxTexture.GetTextureFromVdf(
+                        PhoenixBridge.VdfsPtr,
+                        materialData.texture,
+                        PxTexture.Format.tex_dxt1, PxTexture.Format.tex_dxt5);
+
+                    // No texture found
+                    if (pxTexture == null)
+                    {
+                        Debug.LogWarning($"Texture {materialData.texture} couldn't be found.");
+                        return;
+                    }
+
+                    var format = pxTexture.format.AsUnityTextureFormat();
+                    if (format == 0)
+                    {
+                        Debug.LogWarning($"Format >{pxTexture.format}< is not supported or not yet tested to work with Unity:");
+                        return;
+                    }
+
+                    var texture = new Texture2D((int)pxTexture.width, (int)pxTexture.height, format, (int)pxTexture.mipmapCount, false);
+
+                    texture.name = materialData.texture;
+
+                    for (var i = 0u; i < pxTexture.mipmapCount; i++)
+                    {
+                        texture.SetPixelData(pxTexture.mipmaps[i].mipmap, (int)i);
+                    }
+
+                    texture.Apply();
+
+                    cachedTextures.Add(materialData.texture, texture);
+                    cachedTexture = texture;
                 }
 
-                var texture = new Texture2D((int)pxTexture.width, (int)pxTexture.height, format, (int)pxTexture.mipmapCount, false);
+                material.mainTexture = cachedTexture;
 
-                texture.name = materialData.texture;
-
-                for (var i = 0u; i < pxTexture.mipmapCount; i++)
-                {
-                    texture.SetPixelData(pxTexture.mipmaps[i].mipmap, (int)i);
-                }
-
-                texture.Apply();
-
-                cachedTextures.Add(materialData.texture, texture);
-                cachedTexture = texture;
+                finalMaterials.Add(material);
             }
 
-            material.mainTexture = cachedTexture;
+            meshRenderer.SetMaterials(finalMaterials);
         }
 
         private void PrepareMeshFilter(MeshFilter meshFilter, PxMRMData mrmData)
         {
-            var mesh = new Mesh();
-            meshFilter.mesh = mesh;
-
-            if (mrmData.subMeshes.Length != 1)
-                throw new ArgumentOutOfRangeException("Currently it's only supported to have exact 1 subMesh for VobMRMs.");
-
-            var vertices = mrmData.positions;
-            var triangles = mrmData.subMeshes.First().triangles;
-            var wedges = mrmData.subMeshes.First().wedges;
-
-
             /**
              * Ok, brace yourself:
              * There are three parameters of interest when it comes to creating meshes for items (etc.).
@@ -169,38 +167,63 @@ namespace UZVR.Creator
              *  uvs = [wedge[0].texture], [wedge[2].texture], [wedge[1].texture]
              */
 
-            // Size is predictable:
-            // 1. vertices.count == triangleIndices.size == uv.size
-            // 2. triangles from Phoenix are 3 indices. Therefore final Unity size is *3
-            var preparedVertices = new Vector3[triangles.Length * 3];
-            var preparedTriangles = new int[triangles.Length * 3];
-            var preparedUVs = new Vector2[triangles.Length * 3];
 
-            for (var i = 0; i < triangles.Length; i++)
+            var mesh = new Mesh();
+            meshFilter.mesh = mesh;
+            mesh.subMeshCount = mrmData.subMeshes.Length;
+
+            var verticesAndUvSize = mrmData.subMeshes.Sum(i => i.triangles.Length) * 3;
+            var preparedVertices = new List<Vector3>(verticesAndUvSize);
+            var preparedUVs = new List<Vector2>(verticesAndUvSize);
+
+            // 2-dimensional arrays (as there are segregated by submeshes)
+            var preparedTriangles = new List<List<int>>(mrmData.subMeshes.Length);
+
+            foreach (var subMesh in mrmData.subMeshes)
             {
-                // One triangle is made of 3 elements for Unity. We therefore need to prepare 3 elements within one loop.
-                var preparedIndex = i * 3;
+                var vertices = mrmData.positions;
+                var triangles = subMesh.triangles;
+                var wedges = subMesh.wedges;
 
-                var index1 = wedges[triangles[i].c];
-                var index2 = wedges[triangles[i].b];
-                var index3 = wedges[triangles[i].a];
+                // every triangle is attached to a new vertex.
+                // Therefore new submesh triangles start referencing their vertices with an offset from previous runs.
+                var verticesIndexOffset = preparedVertices.Count;
 
-                preparedVertices[preparedIndex] = vertices[index1.index].ToUnityVector();
-                preparedVertices[preparedIndex + 1] = vertices[index2.index].ToUnityVector();
-                preparedVertices[preparedIndex + 2] = vertices[index3.index].ToUnityVector();
+                var subMeshTriangles = new List<int>(triangles.Length * 3);
+                for (var i = 0; i < triangles.Length; i++)
+                {
+                    // One triangle is made of 3 elements for Unity. We therefore need to prepare 3 elements within one loop.
+                    var preparedIndex = i * 3 + verticesIndexOffset;
 
-                preparedTriangles[preparedIndex] = preparedIndex;
-                preparedTriangles[preparedIndex + 1] = preparedIndex +1;
-                preparedTriangles[preparedIndex + 2] = preparedIndex +2;
+                    var index1 = wedges[triangles[i].c];
+                    var index2 = wedges[triangles[i].b];
+                    var index3 = wedges[triangles[i].a];
 
-                preparedUVs[preparedIndex] = index1.texture.ToUnityVector();
-                preparedUVs[preparedIndex + 1] = index2.texture.ToUnityVector();
-                preparedUVs[preparedIndex + 2] = index3.texture.ToUnityVector();
+                    preparedVertices.Add(vertices[index1.index].ToUnityVector());
+                    preparedVertices.Add(vertices[index2.index].ToUnityVector());
+                    preparedVertices.Add(vertices[index3.index].ToUnityVector());
+
+                    subMeshTriangles.Add(preparedIndex);
+                    subMeshTriangles.Add(preparedIndex + 1);
+                    subMeshTriangles.Add(preparedIndex + 2);
+
+                    preparedUVs.Add(index1.texture.ToUnityVector());
+                    preparedUVs.Add(index2.texture.ToUnityVector());
+                    preparedUVs.Add(index3.texture.ToUnityVector());
+                }
+                preparedTriangles.Add(subMeshTriangles);
             }
 
+            // Unity 1/ handles vertices on mesh level, but triangles (aka vertex-indices) on submesh level.
+            // and 2/ demands vertices to be stored before triangles/uvs.
+            // Therefore we prepare the full data once and assign it afterwards.
+            // @see: https://answers.unity.com/questions/531968/submesh-vertices.html
             mesh.SetVertices(preparedVertices);
-            mesh.SetTriangles(preparedTriangles, 0);
             mesh.SetUVs(0, preparedUVs);
+            for (var i = 0; i < mrmData.subMeshes.Length; i++)
+            {
+                mesh.SetTriangles(preparedTriangles[i], i);
+            }
         }
     }
 }
