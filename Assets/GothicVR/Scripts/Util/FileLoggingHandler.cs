416d418a6a1c57163d6bbedc132886f401d36431
Tyroonis <info@tyroonis.de>
2023-05-12 15:15:03 +0200

    add LogLevel and Systeminformation

--- Assets/GothicVR/Scripts/Util/FileLoggingHandler.cs
+++ Assets/GothicVR/Scripts/Util/FileLoggingHandler.cs
@@ -6,13 +6,25 @@ namespace GVR.Settings
     public class FileLoggingHandler : MonoBehaviour
     {
         private StreamWriter fileWriter;
+        int currLogLevel = 0;
 
-        private void Awake()
+
+		private void Awake()
         {
-            fileWriter = new StreamWriter(Application.persistentDataPath + "/gothicvr_log.txt", false);
-            Application.logMessageReceived += HandleLog;
+			//currLogLevel = SingletonBehaviour<SettingsManager>.GetOrCreate().GameSettings.LogLevel;
+
+			fileWriter = new StreamWriter(Application.persistentDataPath + "/gothicvr_log.txt", false);
+			fileWriter.WriteLine("DeviceModel: " + SystemInfo.deviceModel);
+			fileWriter.WriteLine("DeviceType: " + SystemInfo.deviceType);
+			fileWriter.WriteLine("OperatingSystem: " + SystemInfo.operatingSystem);
+			fileWriter.WriteLine("OperatingSystemFamily: " + SystemInfo.operatingSystemFamily);
+			fileWriter.WriteLine("MemorySize: " + SystemInfo.systemMemorySize);
+			fileWriter.WriteLine("GVR Version: " + Application.version);
+            fileWriter.WriteLine();
+			Application.logMessageReceived += HandleLog;
             Debug.Log("Init file logging handler done");
-        }
+			fileWriter.Flush();
+		}
 
         private void OnDestroy()
         {
@@ -22,11 +34,44 @@ namespace GVR.Settings
 
         private void HandleLog(string logString, string stackTrace, LogType type)
         {
-            fileWriter.WriteLine(type + ": " + logString);
-            if (type == LogType.Exception)
-            {
-                fileWriter.WriteLine(stackTrace);
-            }
-        }
+            var level = (int)type;
+
+			if (level >= currLogLevel)
+			{
+				switch (level)
+				{
+					case (int)LogLevel.Debug:
+						fileWriter.WriteLine(type + ": " + logString);
+						fileWriter.Flush();
+						break;
+					case (int)LogLevel.Info:
+						fileWriter.WriteLine(type + ": " + logString);
+						fileWriter.Flush();
+						break;
+					case (int)LogLevel.Warn:
+						fileWriter.WriteLine(type + ": " + logString);
+						fileWriter.Flush();
+						break;
+					case (int)LogLevel.Error:
+						fileWriter.WriteLine(type + ": " + logString);
+						fileWriter.Flush();
+						break;
+					case (int)LogLevel.Fatal:
+						fileWriter.WriteLine(type + ": " + logString);
+						fileWriter.Flush();
+						break;
+				}
+			}
+
+		}
+    }
+
+    public enum LogLevel
+    {
+        Debug = 0,
+        Info = 1,
+        Warn = 2,
+        Error = 3,
+        Fatal = 4,
     }
 }
