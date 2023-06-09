14db8c1501cb2663a2e1fdae0d8bf4726367a423
Jucan Andrei Daniel <setrofex@gmail.com>
2023-06-09 12:18:36 +0300

    Update ADPCM decoding (just to have the final version saved until migration)

--- Assets/GothicVR/Scripts/Phoenix/Util/SoundConverter.cs
+++ Assets/GothicVR/Scripts/Phoenix/Util/SoundConverter.cs
@@ -34,7 +34,8 @@ namespace GVR.Phoenix.Util
 
             if (audioFormat == 17)
             {
-                return ToAudioClip(AddWavHeader(ImaAdpcmDecoder.Decode(fileBytes, fileBytes.Length * 2), sampleRate, 16, (short)channels), offsetSamples, name);
+                var ima = new ImaAdpcmDecoder();
+                return ToAudioClip(ima.Decode(fileBytes));
             }
             Debug.LogFormat("riff={0} wave={1} subchunk1={2} format={3} channels={4} sampleRate={5} byteRate={6} blockAlign={7} bitDepth={8} headerOffset={9} subchunk2={10} filesize={11}", riff, wave, subchunk1, formatCode, channels, sampleRate, byteRate, blockAlign, bitDepth, headerOffset, subchunk2, fileBytes.Length);
             switch (bitDepth)
@@ -126,106 +127,6 @@ namespace GVR.Phoenix.Util
                     Debug.LogWarning("Unknown wav code format:" + code);
                     return "";
             }
-        }
-
-        public static byte[] AddWavHeader(short[] pcmData, int sampleRate, short bitsPerSample, short channels)
-        {
-            byte[] header = new byte[44];
-
-            // Chunk ID
-            header[0] = (byte)'R';
-            header[1] = (byte)'I';
-            header[2] = (byte)'F';
-            header[3] = (byte)'F';
-
-            // Chunk size
-            int chunkSize = pcmData.Length * (bitsPerSample / 8) * channels + 36;
-            header[4] = (byte)(chunkSize & 0xff);
-            header[5] = (byte)((chunkSize >> 8) & 0xff);
-            header[6] = (byte)((chunkSize >> 16) & 0xff);
-            header[7] = (byte)((chunkSize >> 24) & 0xff);
-
-            // Format
-            header[8] = (byte)'W';
-            header[9] = (byte)'A';
-            header[10] = (byte)'V';
-            header[11] = (byte)'E';
-
-            // Sub-chunk 1 ID
-            header[12] = (byte)'f';
-            header[13] = (byte)'m';
-            header[14] = (byte)'t';
-            header[15] = (byte)' ';
-
-            // Sub-chunk 1 size
-            int subchunk1Size = 16;
-            header[16] = (byte)(subchunk1Size & 0xff);
-            header[17] = (byte)((subchunk1Size >> 8) & 0xff);
-            header[18] = (byte)((subchunk1Size >> 16) & 0xff);
-            header[19] = (byte)((subchunk1Size >> 24) & 0xff);
-
-            // Audio format
-            short audioFormat = 1; // PCM
-            header[20] = (byte)(audioFormat & 0xff);
-            header[21] = (byte)((audioFormat >> 8) & 0xff);
-
-            // Number of channels
-            header[22] = (byte)(channels & 0xff);
-            header[23] = (byte)((channels >> 8) & 0xff);
-
-            // Sample rate
-            header[24] = (byte)(sampleRate & 0xff);
-            header[25] = (byte)((sampleRate >> 8) & 0xff);
-            header[26] = (byte)((sampleRate >> 16) & 0xff);
-            header[27] = (byte)((sampleRate >> 24) & 0xff);
-
-            // Byte rate
-            int byteRate = sampleRate * channels * (bitsPerSample / 8);
-            header[28] = (byte)(byteRate & 0xff);
-            header[29] = (byte)((byteRate >> 8) & 0xff);
-            header[30] = (byte)((byteRate >> 16) & 0xff);
-            header[31] = (byte)((byteRate >> 24) & 0xff);
-
-            // Block align
-            short blockAlign = (short)(channels * (bitsPerSample / 8));
-            header[32] = (byte)(blockAlign & 0xff);
-            header[33] = (byte)((blockAlign >> 8) & 0xff);
-
-            // Bits per sample
-            header[34] = (byte)(bitsPerSample & 0xff);
-            header[35] = (byte)((bitsPerSample >> 8) & 0xff);
-
-            // Sub-chunk 2 ID
-            header[36] = (byte)'d';
-            header[37] = (byte)'a';
-            header[38] = (byte)'t';
-            header[39] = (byte)'a';
-
-            // Sub-chunk 2 size
-            int subchunk2Size = pcmData.Length * (bitsPerSample / 8) * channels;
-            header[40] = (byte)(subchunk2Size & 0xff);
-            header[41] = (byte)((subchunk2Size >> 8) & 0xff);
-            header[42] = (byte)((subchunk2Size >> 16) & 0xff);
-            header[43] = (byte)((subchunk2Size >> 24) & 0xff);
-
-            byte[] wavData = new byte[header.Length + pcmData.Length * (bitsPerSample / 8) * channels];
-
-            Array.Copy(header, wavData, header.Length);
-
-            int offset = header.Length;
-
-            for (int i = 0; i < pcmData.Length; i++)
-            {
-                for (int j = 0; j < channels; j++)
-                {
-                    short sample = pcmData[i];
-                    byte[] bytes = BitConverter.GetBytes(sample);
-                    wavData[offset++] = bytes[0];
-                    wavData[offset++] = bytes[1];
-                }
-            }
-
-            return wavData;
         }
     }
 }
