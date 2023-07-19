4dafcacca5ac18b0f67f9edd8df22ac63317755e
Jucan Andrei Daniel <setrofex@gmail.com>
2023-07-19 19:17:24 +0300

    Refactor LoadWorld to use multiple functions Improves readablity by making the code a bit more modular

--- Assets/GothicVR/Scripts/Manager/GvrSceneManager.cs
+++ Assets/GothicVR/Scripts/Manager/GvrSceneManager.cs
@@ -6,7 +6,6 @@ using GVR.Util;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.UI;
 
 namespace GVR.Manager
 {
@@ -24,6 +23,8 @@ namespace GVR.Manager
 
         public GameObject interactionManager;
 
+        private const int EnsureLoadingBarDelayMilliseconds = 5;
+
         /// <summary>
         /// Called once after bootstrapping scene is done.
         /// Then load either menu or a world defined inside DebugSettings.
@@ -36,42 +37,119 @@ namespace GVR.Manager
 
         public async void LoadWorld(string worldName, string startVob)
         {
+            SceneManagerActionHandler();
+
             await ShowLoadingScene(worldName);
+
             newWorldName = worldName;
             startVobAfterLoading = startVob;
+
+            LoadNewWorldScene(newWorldName);
+            await CreateWorld();
+
+            HideLoadingScene();
+        }
+
+        private void LoadNewWorldScene(string worldName)
+        {
             var newWorldScene = SceneManager.LoadScene(worldName, new LoadSceneParameters(LoadSceneMode.Additive));
 
-            // Remove previous scene.
-            // TODO - it might be, that we need to wait for old map to be removed before loading new one. Let's see...
+            // Remove previous scene if it exists
             if (GameData.I.WorldScene.HasValue)
             {
-                // Try to delete everything in old scene, so we can load everything again from scratch
-                var objects = GameData.I.WorldScene.Value.GetRootGameObjects();
-                for (int i = 0; i < objects.Length; i++)
-                {
-                    GameObject.Destroy(objects[i]);
-                }
                 SceneManager.UnloadSceneAsync(GameData.I.WorldScene.Value);
             }
 
-            newWorldScene = SceneManager.GetSceneByName(newWorldName);
-
             GameData.I.WorldScene = newWorldScene;
+        }
+
+        private async Task CreateWorld()
+        {
+            var newWorldScene = SceneManager.GetSceneByName(newWorldName);
 
             var worldGo = await WorldCreator.I.Create(newWorldName);
-            SceneManager.sceneLoaded += (Scene scene, LoadSceneMode mode) =>
-            {
-                if (scene.name == newWorldName)
-                    SceneManager.SetActiveScene(SceneManager.GetSceneByName(newWorldName));
-            };
-            // Delay for one frame to make sure that the scene can be set active successfully
+
+            // Delay for one frame to allow the scene to be set active successfully
             await Task.Delay(1);
 
             if (worldGo)
             {
                 SceneManager.MoveGameObjectToScene(worldGo, newWorldScene);
                 FindSpot(newWorldScene);
-                HideLoadingScene();
+            }
+        }
+
+        private async Task ShowLoadingScene(string worldName = null)
+        {
+            generalScene = SceneManager.GetSceneByName(generalSceneName);
+            if (generalScene.isLoaded)
+            {
+                SceneManager.MoveGameObjectToScene(interactionManager, SceneManager.GetSceneByName("Bootstrap"));
+                SceneManager.UnloadSceneAsync(generalScene);
+                generalSceneLoaded = false;
+            }
+
+            SetLoadingTextureForWorld(worldName);
+
+            var loadingScene = SceneManager.LoadScene("Loading", new LoadSceneParameters(LoadSceneMode.Additive));
+
+            // Delay for magic number amount to make sure that bar can be found
+            // 1 and 2 caused issues for the 3rd time showing the loading scene in editor
+            await Task.Delay(EnsureLoadingBarDelayMilliseconds);
+        }
+
+        private void SetLoadingTextureForWorld(string worldName)
+        {
+            if (worldName == null)
+                return;
+
+            // set the loading background texture properly
+            // TODO: for new game we need to load texture "LOADING.TGA"
+            var textureString = "LOADING_" + worldName.Split('.')[0].ToUpper() + ".TGA";
+            UIManager.I.SetTexture(textureString, UIManager.I.GothicLoadingMenuMaterial);
+        }
+
+        private void HideLoadingScene()
+        {
+            SceneManager.UnloadSceneAsync("Loading");
+
+            LoadingManager.I.ResetProgress();
+        }
+
+        private void SceneManagerActionHandler()
+        {
+            SceneManager.sceneLoaded += OnWorldSceneLoaded;
+
+            SceneManager.sceneUnloaded += OnLoadingSceneUnloaded;
+        }
+        private void OnWorldSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.name == newWorldName)
+            {
+                SceneManager.SetActiveScene(scene);
+            }
+            if (scene == generalScene)
+            {
+                SceneManager.MoveGameObjectToScene(interactionManager, generalScene);
+
+                WorldCreator.I.PostCreate(interactionManager.GetComponent<XRInteractionManager>());
+
+                var playerParent = scene.GetRootGameObjects().FirstOrDefault(go => go.name == "PlayerController");
+                playerParent.transform.Find("VRPlayer_v4 (romey)").transform.position = startPoint.transform.position;
+            }
+            if (scene.name == "Loading")
+            {
+                LoadingManager.I.SetBarFromScene(scene);
+                LoadingManager.I.SetMaterialForLoading(scene);
+            }
+        }
+
+        private void OnLoadingSceneUnloaded(Scene scene)
+        {
+            if (scene.name == "Loading" && !generalSceneLoaded)
+            {
+                generalScene = SceneManager.LoadScene(generalSceneName, new LoadSceneParameters(LoadSceneMode.Additive));
+                generalSceneLoaded = true;
             }
         }
 
@@ -95,69 +173,6 @@ namespace GVR.Manager
                     }
                 }
             }
-        }
-
-        private async Task ShowLoadingScene(string worldName = null)
-        {
-            generalScene = SceneManager.GetSceneByName(generalSceneName);
-            if (generalScene.isLoaded)
-            {
-                SceneManager.MoveGameObjectToScene(interactionManager, SceneManager.GetSceneByName("Bootstrap"));
-                SceneManager.UnloadSceneAsync(generalScene);
-                generalSceneLoaded = false;
-            }
-
-            // set the loading background texture properly
-            if (worldName != null)
-            {
-                // TODO: for new game we need to load texture "LOADING.TGA"
-                var textureString = "LOADING_" + worldName.Split('.')[0].ToUpper() + ".TGA";
-                UIManager.I.SetTexture(textureString, UIManager.I.GothicLoadingMenuMaterial);
-            }
-
-            var loadingScene = SceneManager.LoadScene("Loading", new LoadSceneParameters(LoadSceneMode.Additive));
-
-            SceneManager.sceneLoaded += (Scene scene, LoadSceneMode mode) =>
-            {
-                if (scene == loadingScene)
-                {
-                    LoadingManager.I.SetBarFromScene(scene);
-                    LoadingManager.I.SetMaterialForLoading(loadingScene);
-                }
-            };
-            // Delay for magic number amount to make sure that bar can be found
-            // 1 and 2 caused issues for the 3rd time showing the loading scene in editor
-            await Task.Delay(5);
-        }
-
-        private void HideLoadingScene()
-        {
-            SceneManager.UnloadSceneAsync("Loading");
-
-            SceneManager.sceneUnloaded += (Scene scene) =>
-            {
-                //this is a fix to make sure that we load General Scene only once
-                if (scene.name == "Loading" && !generalSceneLoaded)
-                {
-                    generalScene = SceneManager.LoadScene(generalSceneName, new LoadSceneParameters(LoadSceneMode.Additive));
-                    generalSceneLoaded = true;
-                }
-            };
-
-            SceneManager.sceneLoaded += (Scene scene, LoadSceneMode mode) =>
-            {
-                if (scene == generalScene)
-                {
-                    SceneManager.MoveGameObjectToScene(interactionManager, generalScene);
-
-                    WorldCreator.I.PostCreate(interactionManager.GetComponent<XRInteractionManager>());
-
-                    var playerParent = scene.GetRootGameObjects().FirstOrDefault(go => go.name == "PlayerController");
-                    playerParent.transform.Find("VRPlayer_v4 (romey)").transform.position = startPoint.transform.position;
-
-                    return;
-                }
-            };
         }
     }
 }
