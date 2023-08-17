302011fab4a949c16226db366e28f97deed84757
Jucan Andrei Daniel <setrofex@gmail.com>
2023-08-17 19:55:37 +0300

    add more menu functions

--- Assets/GothicVR/Scripts/Player/Menu/MenuButtonScript.cs
+++ Assets/GothicVR/Scripts/Player/Menu/MenuButtonScript.cs
@@ -5,9 +5,61 @@ namespace GVR.Player.Menu
 {
     public class MenuButtonScript : MonoBehaviour
     {
+
+        public GameObject MainMenu;
+        public GameObject SettingsMenu;
+        public GameObject TeleportMenu;
+        public GameObject MovementMenu;
+        public GameObject UIMenu;
+
         public void PlayFunction()
         {
             GvrSceneManager.I.LoadWorld(ConstantsManager.I.selectedWorld, ConstantsManager.I.selectedWaypoint);
+        }
+
+        public void ShowMainMenu()
+        {
+            MainMenu.SetActive(true);
+            SettingsMenu.SetActive(false);
+            TeleportMenu.SetActive(false);
+            MovementMenu.SetActive(false);
+            UIMenu.SetActive(false);
+        }
+
+        public void ShowSettingsMenu()
+        {
+            SettingsMenu.SetActive(true);
+            MainMenu.SetActive(false);
+            TeleportMenu.SetActive(false);
+            MovementMenu.SetActive(false);
+            UIMenu.SetActive(false);
+        }
+
+        public void ShowTeleportMenu()
+        {
+            TeleportMenu.SetActive(true);
+            MainMenu.SetActive(false);
+            SettingsMenu.SetActive(false);
+            MovementMenu.SetActive(false);
+            UIMenu.SetActive(false);
+        }
+
+        public void ShowMovementMenu()
+        {
+            MovementMenu.SetActive(true);
+            MainMenu.SetActive(false);
+            SettingsMenu.SetActive(false);
+            TeleportMenu.SetActive(false);
+            UIMenu.SetActive(false);
+        }
+
+        public void ShowUIMenu()
+        {
+            UIMenu.SetActive(true);
+            MainMenu.SetActive(false);
+            SettingsMenu.SetActive(false);
+            TeleportMenu.SetActive(false);
+            MovementMenu.SetActive(false);
         }
 
         public void QuitGameFunction()
