6dc1dbfe93045cb3b050375d49e15fec4a2c3342
Jucan Andrei Daniel <setrofex@gmail.com>
2023-11-07 20:45:24 +0200

    add ToAdditiveMode and fix TransparentMode

--- Assets/GothicVR/Scripts/Extensions/MaterialExtension.cs
+++ Assets/GothicVR/Scripts/Extensions/MaterialExtension.cs
@@ -54,9 +54,23 @@ namespace GVR.Extensions
             material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
             material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
             material.SetInt("_ZWrite", 0);
-            material.DisableKeyword("_ALPHATEST_ON");
+            material.SetFloat("_SurfaceType", 1f);
+            material.EnableKeyword("_ALPHATEST_ON");
             material.EnableKeyword("_ALPHABLEND_ON");
-            material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+            material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
+        }
+
+        public static void ToAdditiveMode(this Material material)
+        {
+            material.SetOverrideTag("RenderType", "Transparent");
+            material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.One);
+            material.SetInt("_ZWrite", 0);
+            material.SetFloat("_SurfaceType", 1f);
+            material.EnableKeyword("_ALPHATEST_ON");
+            material.EnableKeyword("_ALPHABLEND_ON");
+            material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
             material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
         }
     }
