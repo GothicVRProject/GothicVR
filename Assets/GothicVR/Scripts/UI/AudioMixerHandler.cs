bb6e8d99d676b131bccb90741ae859a2dac556c6
Jucan Andrei Daniel <setrofex@gmail.com>
2023-11-12 19:33:44 +0200

    Create Script to handle volume sliders

---
+++ Assets/GothicVR/Scripts/UI/AudioMixerHandler.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+namespace GVR.GothicVR.Scripts.UI
+{
+    public class AudioMixerHandler : MonoBehaviour
+    {
+        [SerializeField] private AudioMixerGroup audioMixer;
+        [SerializeField] private Slider audioVolumeSlider;
+        [SerializeField] private string volumePlayerPrefName;
+
+        void onEnable()
+        {
+            float oldVolume = PlayerPrefs.GetFloat(volumePlayerPrefName);
+            audioVolumeSlider.value = oldVolume;
+        }
+
+        public void SliderUpdate(float value)
+        {
+            PlayerPrefs.SetFloat(volumePlayerPrefName, value);
+            audioMixer.audioMixer.SetFloat(volumePlayerPrefName, Mathf.Log10(value) * 20);
+        }
+    }
+}
