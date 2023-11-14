4bff631c8f479334a212443efd9dc073c351a29d
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2023-11-14 08:56:08 +0100

    Lab loading of Chest and Door.

--- Assets/GothicVR-Lab/Scripts/Handler/VobHandAttachPointsHandler.cs
+++ Assets/GothicVR-Lab/Scripts/Handler/VobHandAttachPointsHandler.cs
@@ -44,8 +44,6 @@ namespace GVR.Lab.Handler
         private Dictionary<string, PxVmItemData> pxItems = new();
         private VobItemAttachPoints attachPoints;
 
-
-
         private void Start()
         {
             /*
@@ -54,14 +52,6 @@ namespace GVR.Lab.Handler
              * 3. Load Vob name list
              * 4. Fill dropdown
              */
-            PhoenixBootstrapper.SetLanguage();
-
-            var vdfsPath = Path.GetFullPath($"{SettingsManager.GameSettings.GothicIPath}/Data");
-            GameData.VfsPtr = VfsBridge.LoadVfsInDirectory(vdfsPath);
-
-            var gothicVmPath = Path.GetFullPath($"{SettingsManager.GameSettings.GothicIPath}/_work/DATA/scripts/_compiled/GOTHIC.DAT");
-            GameData.VmGothicPtr = VmGothicExternals.LoadVm(gothicVmPath);
-
             List<string> itemNames = new();
             PxVm.pxVmEnumerateInstancesByClassName(GameData.VmGothicPtr, "C_Item", (string name) => itemNames.Add(name));
 
