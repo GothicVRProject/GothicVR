9023fed098b1a46ebe74a645260321a978eabaa8
BoroBongo <43158992+BoroBongo@users.noreply.github.com>
2023-11-18 21:16:10 +0000

    added some more comments

--- Assets/GothicVR/Scripts/World/DijkstraWayPointCreator.cs
+++ Assets/GothicVR/Scripts/World/DijkstraWayPointCreator.cs
@@ -21,18 +21,22 @@ namespace GVR
             var wayEdges = world.waypointEdges;
             var wayPoints = world.waypoints;
 
-            // Refactored using Linq 
+            // Using LINQ to transform wayEdges into DijkstraWaypoints.
             DijkstraWaypoints = wayEdges.SelectMany(edge => new[]
             {
+                // For each edge, create two entries: one for each direction of the edge.
+                // 'a' is the source waypoint, 'b' is the destination waypoint.
                 new { a = wayPoints[(int)edge.a], b = wayPoints[(int)edge.b] },
                 new { a = wayPoints[(int)edge.b], b = wayPoints[(int)edge.a] }
             })
-            .GroupBy(x => x.a.name)
-            .ToDictionary(g => g.Key, g => new DijkstraWaypoint(g.Key)
+            .GroupBy(x => x.a.name) // Group the entries by the name of the source waypoint.
+            .ToDictionary(g => g.Key, g => new DijkstraWaypoint(g.Key) // Transform each group into a DijkstraWaypoint.
             {
+                // The neighbors of the DijkstraWaypoint are the names of the destination waypoints in the group.
                 Neighbors = g.Select(x => x.b.name).ToList()
             });
 
+            // Set the DijkstraWaypoints in the DijkstraPathFinder instance.
             DijkstraPathFinder.Instance.SetDijkstraWaypoints(DijkstraWaypoints);
 
             return DijkstraWaypoints;
