5b7831c8415a8ec5d5bcf07290530557631414c2
BoroBongo <43158992+BoroBongo@users.noreply.github.com>
2023-11-18 02:18:09 +0000

    added some comments to DijkstraWaypoints

--- Assets/GothicVR/Scripts/World/DijkstraWaypoint.cs
+++ Assets/GothicVR/Scripts/World/DijkstraWaypoint.cs
@@ -8,19 +8,20 @@ namespace GVR
 {
     public class DijkstraWaypoint : IComparable<DijkstraWaypoint>
     {
-        public string Name = "";                                //as index to find other data like position, underwater and probably isFree
+        public string Name = "";  // Used as index to find other data like position, underwater and probably isFree
 
-        public double CoveredDistance = 0;
+        public double CoveredDistance = 0;  // Initial distance covered from the source node is 0
 
-        public double SummedDistance = 99999;
-        
-        public Dictionary<string, float> DistanceToNeighbors = new Dictionary<string, float>();
+        public double SummedDistance = 99999;  // Initialized to a large number to represent infinity at the start of the algorithm. 
+                                               // This is used for the priority queue to determine which node to visit next (smaller distance = higher priority)
 
-        public List<string> Neighbors = new List<string>();
+        public Dictionary<string, float> DistanceToNeighbors = new Dictionary<string, float>();  // Stores the distances to neighboring nodes
 
-        public Vector3 Position = new Vector3();
+        public List<string> Neighbors = new List<string>();  // Stores the neighboring nodes
 
-        public string FatherWP = "";
+        public Vector3 Position = new Vector3();  // Stores the position of the node
+
+        public string FatherWP = "";  // Stores the previous node in the shortest path
 
         public DijkstraWaypoint(string name)
         {
