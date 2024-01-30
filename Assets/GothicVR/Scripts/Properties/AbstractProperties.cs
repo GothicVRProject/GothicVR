c165efa35263d9e3ce5120037c0a1d30b0382005
Jucan Andrei Daniel <setrofex@gmail.com>
2024-01-30 15:12:19 +0200

    fix(AbstractProperties): Remove custom gameobject

--- Assets/GothicVR/Scripts/Properties/AbstractProperties.cs
+++ Assets/GothicVR/Scripts/Properties/AbstractProperties.cs
@@ -4,21 +4,6 @@ namespace GVR.Properties
 {
     public abstract class AbstractProperties : MonoBehaviour
     {
-        // using this to store the hero gameobject as monobehaviour has gameObject as readonly
-        // I did this because it's easier than to recreate the properties on every world transition
-        private GameObject customGameObject; 
-
-        public GameObject go
-        {
-            get
-            {
-                if (customGameObject != null)
-                    return customGameObject;
-
-                return gameObject;
-            }
-            // FIXME - Seems this is never called. Let's see if we can remove it later safely.
-            set => customGameObject = value;
-        }
+        public GameObject go => gameObject;
     }
 }
